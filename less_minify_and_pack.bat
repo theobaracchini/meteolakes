f4556961a8c3a2e67945ef35c198a71eadea4d69
Roman Zoller <roman.zoller@epfl.ch>
2016-03-04 15:29:34 +0100

    Remove .js files from pack script

--- less_minify_and_pack.bat
+++ less_minify_and_pack.bat
@@ -10,32 +10,6 @@ type css\bootstrap.less.css > css\style.min.css
 echo. >> css\style.min.css
 ..\jsmin < css\style.css >> css\style.min.css
 
-:: JS
-
-type globals.js > code.js
-echo. >> code.js
-type models\dateplusplus.js >> code.js
-echo. >> code.js
-type models\temporalData.js >> code.js
-echo. >> code.js
-type models\chart.js >> code.js
-echo. >> code.js
-type models\Particle.js >> code.js
-echo. >> code.js
-type models\ParticleEmitter.js >> code.js
-echo. >> code.js
-type app.js >> code.js
-echo. >> code.js
-type temperatureCtrl.js >> code.js
-echo. >> code.js
-type velocityCtrl.js >> code.js
-echo. >> code.js
-type timeCtrl.js >> code.js
-echo. >> code.js
-
-..\jsmin < code.js > code.min.js
-
-
 :: Delete the old archive
 del package.zip
 
