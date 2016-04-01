abed88a926c21c6a76b0af1420203bc99caa9902
Roman Zoller <roman.zoller@epfl.ch>
2016-04-01 15:10:59 +0200

    Refactor rbush-knn into an angular module

--- app/vendor/js/rbush/rbush-knn.js
+++ app/vendor/js/rbush/rbush-knn.js
@@ -1,53 +1,51 @@
-'use strict';
+angular.module('rbush').factory('knn', function(TinyQueue) {
+    function knn(tree, queryPoint, n, predicate) {
+        var node = tree.data,
+            result = [],
+            toBBox = tree.toBBox,
+            i, child;
 
-var Queue = require('tinyqueue');
+        var queue = new TinyQueue(null, compareDist);
 
-module.exports = knn;
+        while (node) {
+            for (i = 0; i < node.children.length; i++) {
+                child = node.children[i];
+                queue.push({
+                    node: child,
+                    isItem: node.leaf,
+                    dist: boxDist(queryPoint, node.leaf ? toBBox(child) : child.bbox)
+                });
+            }
 
-function knn(tree, queryPoint, n, predicate) {
-    var node = tree.data,
-        result = [],
-        toBBox = tree.toBBox,
-        i, child;
+            while (queue.length && queue.peek().isItem) {
+                var candidate = queue.pop().node;
+                if (!predicate || predicate(candidate))
+                    result.push(candidate);
+                if (result.length === n) return result;
+            }
 
-    var queue = new Queue(null, compareDist);
-
-    while (node) {
-        for (i = 0; i < node.children.length; i++) {
-            child = node.children[i];
-            queue.push({
-                node: child,
-                isItem: node.leaf,
-                dist: boxDist(queryPoint, node.leaf ? toBBox(child) : child.bbox)
-            });
+            node = queue.pop();
+            if (node) node = node.node;
         }
 
-        while (queue.length && queue.peek().isItem) {
-            var candidate = queue.pop().node;
-            if (!predicate || predicate(candidate))
-                result.push(candidate);
-            if (result.length === n) return result;
-        }
-
-        node = queue.pop();
-        if (node) node = node.node;
+        return result;
     }
 
-    return result;
-}
+    function compareDist(a, b) {
+        return a.dist - b.dist;
+    }
 
-function compareDist(a, b) {
-    return a.dist - b.dist;
-}
+    function boxDist(p, box) {
+        var dx = axisDist(p[0], box[0], box[2]),
+            dy = axisDist(p[1], box[1], box[3]);
+        return dx * dx + dy * dy;
+    }
 
-function boxDist(p, box) {
-    var dx = axisDist(p[0], box[0], box[2]),
-        dy = axisDist(p[1], box[1], box[3]);
-    return dx * dx + dy * dy;
-}
+    function axisDist(k, min, max) {
+        return k < min ? min - k :
+               k <= max ? 0 :
+               k - max;
+    }
 
-function axisDist(k, min, max) {
-    return k < min ? min - k :
-           k <= max ? 0 :
-           k - max;
-}
+    return knn;
+});
