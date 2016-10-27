e47fa723bebccdc06762a8e066d8dfb7e2237192
Martin Zoller <epfl@spam.zoller.tv>
2016-10-27 17:20:13 +0100

    Integrate template of loading indicator in JS to avoid deployment complications

--- app/js/directives/Loading.js
+++ app/js/directives/Loading.js
@@ -1,6 +1,6 @@
 angular.module('lakeViewApp').directive('loading', function() {
     return {
         restrict: 'E',
-        templateUrl: 'app/templates/loading.template.html'
+        template: '<div class="loading"><div class="sk-folding-cube"><div class="sk-cube1 sk-cube"></div><div class="sk-cube2 sk-cube"></div><div class="sk-cube4 sk-cube"></div><div class="sk-cube3 sk-cube"></div> </div></div>'
     };
 });
