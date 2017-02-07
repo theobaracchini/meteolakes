814059e753eef32c7a619870f3695b9b8923b6eb
Martin Zoller <epfl@spam.zoller.tv>
2017-02-07 14:52:01 +0000

    Fix site navigation: Auto-highlight current page and remove href from internal JavaScript links.

--- app/app.module.js
+++ app/app.module.js
@@ -3,4 +3,4 @@
 // Prevent pixi.js from spamming the dev console
 PIXI.utils._saidHello = true;
 
-angular.module('lakeViewApp', ['rbush', 'stats', 'ngAnimate', 'ngRoute']);
+angular.module('lakeViewApp', ['autoActive', 'rbush', 'stats', 'ngAnimate', 'ngRoute']);
---
+++ app/auto-active/auto-active.js
@@ -0,0 +1,25 @@
+angular.module('autoActive', []).directive('autoActive', ['$location', function($location) {
+    return {
+        restrict: 'A',
+        scope: false,
+        link: function(scope, element) {
+            function setActive() {
+                var path = $location.path();
+                console.log('path:'+path);
+                if (path) {
+                    angular.forEach(element.find('li'), function(li) {
+                        var anchor = li.querySelector('a');
+                        if (anchor.href.match('#!' + path + '(?=\\?|$)')) {
+                            angular.element(li).addClass('active');
+                        } else {
+                            angular.element(li).removeClass('active');
+                        }
+                    });
+                }
+            }
+
+            setActive();
+            scope.$on('$locationChangeSuccess', setActive);
+        }
+    };
+}]);
