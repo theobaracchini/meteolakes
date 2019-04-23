074480ce6063b1b1df4bc820209889596c37cd59
theobaracchini <theo.baracchini@epfl.ch>
2019-04-23 13:03:57 +0200

    Added info page for messages

---
+++ app/page-info/page-info.component.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Register `model` component, along with its associated controller and template
+angular.module('meteolakesApp').component('pageInfo', {
+    templateUrl: 'app/page-info/page-info.template.html',
+    controller: function() {
+
+    }
+});
