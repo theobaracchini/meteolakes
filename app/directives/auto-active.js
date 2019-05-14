18c3e98cac8103446002a223e6c736259caac743
Theo Baracchini <theo.baracchini@epfl.ch>
2019-05-14 17:32:14 +0200

    Fixed an issue with the active tab in hydro and waq (due to lakeID in address path + update to version 1.5.2

--- app/directives/auto-active.js
+++ app/directives/auto-active.js
@@ -8,6 +8,11 @@ angular.module('meteolakesApp').directive('autoActive', ['$location', function($
                 if (path) {
                     angular.forEach(element.find('li'), function(li) {
                         var anchor = li.querySelector('a');
+                        if (path.indexOf('/hydro/') >= 0) {
+                            path = '/hydro';
+                        } else if (path.indexOf('/quality/') >= 0){
+                            path = '/quality';
+                        }
                         if (anchor.href.match('#!' + path + '(?=\\?|$)')) {
                             angular.element(li).addClass('active');
                         } else {
